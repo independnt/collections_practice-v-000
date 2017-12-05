1492a3da64cd3eda56942924d5615fc75b99e31b
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-05 16:31:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,8 +27,5 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array.map do |a|
-    array[1] = array[2]
-    array[2] = array[1]
-  end
+  array[1], array[2] = array[2],array[1]
 end
